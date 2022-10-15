4550efe26791de2f900e5bb72fecf3ca456ce534
Dawid Maroń <daw.maron@gmail.com>
2022-10-15 04:22:49 +0200

    Create Task5.java

---
+++ src/Task5.java
@@ -0,0 +1,23 @@
+      /*
+       5.Napisz program
+        generujący jednowymiarową tablicę (10 elementów) liczb w której każda liczba jest losowa ale nie jest mniejsza
+        niż poprzednia liczba. Przykłady:
+        23, 34, 53, 60, 80
+        0, 10, 70, 70, 70
+        nieprawidłowy wynik:
+        34, 56, 34, 78, 99 - trzecia liczba jest mniejsza niż poprzednia (druga).
+        Wynikiem działania programu ma być wyświetlona wygenerowana tablica. Zakres losowych
+        liczb 0 - 20.
+        */
+
+      /*
+      PLAN
+      1. Create tab[10]
+      2. Fill tab with randoms (next number is even or higher than previous) - (0 - 20)
+      3. Print tab
+      */
+public class Task5 extends Utils {
+    public static void main(String[] args) {
+
+    }
+}
