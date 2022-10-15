758372524d1d7684a340b81e6d5165d681a8732d
Dawid Maroń <daw.maron@gmail.com>
2022-10-15 05:59:19 +0200

    Create Task7.java

---
+++ src/Task7.java
@@ -0,0 +1,18 @@
+/*
+        7.Napisz program który generuje jednowymiarową tablicę (10 elementów) losowych liczb. Wynikiem działania
+        programu ma być wygenerowana tablica oraz ilość powtórzeń liczby 5. Zakres losowych liczb 0 - 10.
+*/
+/*
+        PLAN:
+        1. Create tab[10]
+        2. Fill tab with randoms (0-10)
+        3. Print tab
+        4. Print number of '5' copies
+
+*/
+
+public class Task7 extends Utils{
+    public static void main(String[] args) {
+
+    }
+}
