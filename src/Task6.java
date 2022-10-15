3bc34c9e1e5d83b1238ca42e6cab971c56798b9b
Dawid Maroń <daw.maron@gmail.com>
2022-10-15 05:55:30 +0200

    Finish Task6.java

--- src/Task6.java
+++ src/Task6.java
@@ -25,8 +25,31 @@
 
 
 public class Task6 {
+    static int w = 11;
+
     public static void main(String[] args) {
+        printChristmassTree(w);
+    }
 
-
+    private static void printChristmassTree (int height) {
+        String body = "*";
+        String background = " ";
+        String row;
+        int rowSize = (height * 2) + 1;
+        // main loop, height + 1 for base
+        for (int i = 0; i < height + 1; i++) {
+            // reset row String
+            row = "";
+            // inner loop, row build
+            for (int j = 0; j < rowSize; j++) {
+                // hardest part here
+                if (i + j >= height && i + j < height + 1 + i*2) {
+                    row += body;
+                }
+                else
+                row += background;
+            }
+            System.out.println(row);
+        }
     }
 }
