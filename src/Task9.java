405456e6d4c8fd65b89b2640734a22a2150621b9
Dawid Maroń <daw.maron@gmail.com>
2022-10-15 06:23:11 +0200

    Create Task9.java

---
+++ src/Task9.java
@@ -0,0 +1,25 @@
+        /*
+        9.Napisz program który generuje jednowymiarową tablicę (10 elementów) losowych liczb. Program ma wygenerować druga
+        tablicę która jest pierwszą tablicą w odwrotnaj kolejności. Wynikiem działania programu ma być wyświetlona pierwsza i
+        druga tablica. Przykłady:
+        3, 23, 2, 23, 53
+        53, 23, 2, 23, 345, 23, 65, 1, 54
+        54, 1, 65, 23, 45
+        Zakres losowych liczb 0 - 100.
+        */
+
+        /*
+        PLAN
+        1. Create tab1 [10]
+        2. Fill tab with random
+        3. Create tab2
+        4. Fill it with tab1 in reverse
+        5. Print tab1
+        6. Print tab2
+
+        */
+public class Task9 {
+    public static void main(String[] args) {
+
+    }
+}
