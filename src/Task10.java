effb2c4b81d1eb7d90d5a185a07403c0bf09d8a0
Dawid Maroń <daw.maron@gmail.com>
2022-10-15 06:39:07 +0200

    Create Task10.java

---
+++ src/Task10.java
@@ -0,0 +1,23 @@
+/*
+        10.Napisz program który generuje dwie tablice (10 elementów) losowych liczb. Wynikiem
+        działania programu mają być wyświetlone obie tablice oraz trzecia tablica będąca połączeniem dwóch wygenerowancyh.
+        Przykład:
+        3, 5, 12, 34, 45
+        45, 21, 65, 23, 1
+        3, 5, 12, 34, 45, 45, 21, 65, 23, 1
+*/
+
+/*
+        PLAN
+        1. Create tab1, tab2, tab3
+        2. Fill tab1, tab2 with randoms
+        3. Merge tab1 and tab2 into tab3
+        4. Print tab1, tab2, tab3
+*/
+
+
+public class Task10 extends Utils {
+    public static void main(String[] args) {
+
+    }
+}
