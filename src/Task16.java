36a89edc0bd77ca90be8f7eebc9f529b1b118a18
marex333 <daw.maron@gmail.com>
2022-10-15 12:23:48 +0200

    Finish Task16.java

--- src/Task16.java
+++ src/Task16.java
@@ -1,4 +1,5 @@
-        /*6.Napisz program
+        /*6.
+        Napisz program
         który generuje tablicę liczb 0 i 1 (10 elementów). Program ma sprawdzić czy tablica jest symetryczna -
         pierwszy element taki jak ostatni, drugi taki jak przedostatni itd. Wynikime działania programu ma być
         wypisana wygenerowana tablica oraz true albo fale w zależności czy jest symetria czy nie.*/
@@ -6,7 +7,22 @@
 
 
 public class Task16 extends Utils {
+    private static int tabSize = 10;
+    private static int biggestNumber = 1;
     public static void main(String[] args) {
+        int[] tab = createTab(tabSize);
+        tab = fillTabWithRandoms(tab, biggestNumber);
+        showTab(tab);
+        System.out.println("Is symetric? : " + isSymetric(tab));
+
+    }
+
+    private static boolean isSymetric(int[] tab) {
+
+        for (int i = 0; i < tab.length / 2; i++) {
+            if(tab[i] != tab[tab.length - 1 - i]) return false;
+        }
+        return true;
 
     }
 }
