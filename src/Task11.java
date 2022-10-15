7b805272451ca5e83e23af03152f4376725ff461
Dawid Maroń <daw.maron@gmail.com>
2022-10-15 06:56:28 +0200

    Create Task11.java

---
+++ src/Task11.java
@@ -0,0 +1,21 @@
+/*      11.Napisz program który losuje dwaie tablice (10 elementów, zakres 0 - 100).
+        Wynikiem działania programu mają być wypisane obie tablice, oraz trzecia tablica w które każdy element jest sumą
+        odpowiadających elementów z dwóch pierwszych tablic.
+        Przykład:
+        1 4 4 64 45
+        23 32 5 23 324 36 9 87 48
+        */
+
+        /*
+        PLAN
+        1. Create tab1, tab2, tab3 [10]
+        2. Fill tab1, tab2 with random (0 - 100)
+        3. Sum up each iteration at tab1 and tab2 and put that into tab3
+        4. Print tab1, 2, 3
+
+        */
+public class Task11 extends Utils {
+    public static void main(String[] args) {
+
+    }
+}
