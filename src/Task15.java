b4c02fa5d2e484277fec20d7b195768b0904f24d
marex333 <daw.maron@gmail.com>
2022-10-15 10:58:08 +0200

    Create Task15.java

---
+++ src/Task15.java
@@ -0,0 +1,12 @@
+       /* 15.Napisz program
+        który symuluje rzuty dwiema kastkami do gry (takie zwykłe białe kości
+        jak w monopoly). Program ma wykońać 10 000 rzutów i wypisać liczbę która wypadała najczęściej
+        (ta liczba jest occzywiście sumą oczek na dwóch kościach które symulujemy.
+*/
+
+
+public class Task15 {
+    public static void main(String[] args) {
+
+    }
+}
