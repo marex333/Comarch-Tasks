4c6ba4b0207782a0d64e6241325ee0c57bd266ab
Dawid Maroń <daw.maron@gmail.com>
2022-10-15 06:12:58 +0200

    Create Task 8.java

---
+++ src/Task8.java
@@ -0,0 +1,29 @@
+/*
+     8.Wypisz na ekranie
+     liczby od 0 do 99. Jeśli liczba jest podzilena przez 3 obok liczny dopisz "Fizz", jeśli liczba jest podzielna przez 5
+     obok liczby dopisz "Buzz", jeśli liczba jest podzielna przez 3 i 5 obok liczby dopisz "Fizz Buzz".
+     Przykład:
+    1
+    2
+    3 Fizz
+    4
+    5 Buzz
+    6 Fizz
+    7
+    8
+    9 Fizz
+    10
+    11
+    12 Fizz
+    13
+    14
+    15 Fizz Buzz
+    16
+    itd.
+
+
+*/
+public class Task8 extends Utils{
+    public static void main(String[] args) {
+    }
+}
