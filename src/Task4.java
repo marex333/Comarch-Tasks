e777db0f2170c94e656b0f80d52d0304c4d64123
Dawid Maroń <daw.maron@gmail.com>
2022-10-15 03:36:47 +0200

    Create Task4.java

---
+++ src/Task4.java
@@ -0,0 +1,9 @@
+       /*
+        4.Napisz program generujący dwuwymiarową tablice (5x5) losowych liczb. Wynikiem działania programu
+        ma być wyświetlona tablica oraz najmniejsza liczba z tej tablicy. Zakres losowych liczb 0 - 100.
+        */
+public class Task4 extends Utils {
+    public static void main(String[] args) {
+
+    }
+}
