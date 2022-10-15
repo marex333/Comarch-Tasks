47074ca275dabb1796f5365e20b1b1e52de6aebd
Dawid Maroń <daw.maron@gmail.com>
2022-10-15 04:41:07 +0200

    Create Task6.java

---
+++ src/Task6.java
@@ -0,0 +1,32 @@
+/*
+        6.Napisz program w którym jest zmienna 'w'. Zmienna ta oznacza wysokość choinki.
+        Program wyświetla na ekranie wypisaną choinkę o danej wysokości.
+        Przykłady:
+        dla w = 3 program wypisuje
+          *
+         ***
+        *****
+        pierwszy wiersz to: spacja spacja gwiazdka spacja spacja
+        drugi wiersz to: spacja gwiazdka gwiazdka gwiazdka spacja
+        trzeci wiersz to: gwiazdka gwiazdka gwiazdka gwiazdka gwiazdka
+        dla w = 5
+            *
+           ***
+          *****
+         *******
+        *********
+
+*/
+/*
+        PLAN
+        1. Print christmass Tree with h =  w - variable
+
+*/
+
+
+public class Task6 {
+    public static void main(String[] args) {
+
+
+    }
+}
