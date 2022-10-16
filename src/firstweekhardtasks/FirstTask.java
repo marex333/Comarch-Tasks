4d1962846ab2eab6d663bc508c996bb54c9a1624
marex333 <daw.maron@gmail.com>
2022-10-16 16:05:18 +0200

    finish FirstTask.java

--- src/firstweekhardtasks/FirstTask.java
+++ src/firstweekhardtasks/FirstTask.java
@@ -1,4 +1,28 @@
+/*1.Napisz program w którym zdefiniowana jest zmienna n. Program ma wyświetlać n elementów ciągu Fibbonaciego. Co to jest ciąg Fibbonaciego znajdziecie na wikipedii. Przykład:
+        n = 3
+        0 1 1
+        n = 8
+        0 1 1 2 3 5 8 13
+*/
 package firstweekhardtasks;
-
 public class FirstTask {
+    private static int n = 11;
+    public static void main(String[] args) {
+    for (int element : fibbonachiSequence(n)) {
+        System.out.println(element);
+    }
+
+    }
+    private static int[] fibbonachiSequence(int length) {
+        int[] tab = new int[length];
+        for (int i = 0; i < tab.length; i++) {
+            if (i == 0) tab[0] = 0;
+            else if (i == 1) tab[1] = 1;
+            else {
+                tab[i] = tab[i-1] + tab[i-2];
+            }
+
+        }
+        return tab;
+    }
 }
